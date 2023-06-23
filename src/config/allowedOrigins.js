e5d4dbd38ec0f71bf2cdcef9d6473e0a68467900
Eudys Collado <eudys1.42@gmail.com>
2023-06-23 01:55:49 -0400

    cors

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://649532b2efda0b3f2292227e--dynamic-baklava-df9725.netlify.app/"
+    "https://649532b2efda0b3f2292227e--dynamic-baklava-df9725.netlify.app"
 ];
 
 module.exports = allowedOrigins;
