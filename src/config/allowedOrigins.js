e5b67ccaf47c82247ae2e462f70931a54ce49f73
Eudys Collado <eudys1.42@gmail.com>
2023-06-28 11:37:42 -0400

    cors updated

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://649c528f2402b820a462fae7--spontaneous-marigold-9d5d74.netlify.app"
+    "https://spontaneous-marigold-9d5d74.netlify.app/"
 ];
 
 module.exports = allowedOrigins;
