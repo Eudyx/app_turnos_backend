322313597454f663900bff38cfbf560b41e1a41c
Eudys Collado <eudys1.42@gmail.com>
2023-06-28 11:40:18 -0400

    alloweOrigins updated

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://spontaneous-marigold-9d5d74.netlify.app/"
+    "https://spontaneous-marigold-9d5d74.netlify.app"
 ];
 
 module.exports = allowedOrigins;
